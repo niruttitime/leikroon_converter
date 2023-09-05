af46f6a65fd6056bf534d50196c38b1c60d17742
niruttitime <61222669+niruttitime@users.noreply.github.com>
2023-09-05 17:31:40 -0400

    v0.2
    implement csv reading; implement mode selector add the csv file;

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import pprint as pp
+import csv
+import sys
 
-hs_baseline = {
-    "CLASS_YEAR": 2020,
-    "AC": 134,
-    "PDS": 62,
-    "EPTS": 18,
-    "CD": 50
-}
+hs_baseline = {}
+
+with open('hs_baseline.csv', 'r') as csvfile:
+    csv_reader = csv.reader(csvfile, delimiter='\t')
+    for row in csv_reader:
+        hs_baseline[row[0].upper()] = int(row[1])
 
 def hs(institution, roon):
     ref_dif = hs_baseline[institution] - roon
@@ -19,8 +20,24 @@ def hs_driver(input):
     parsed_input = input.split()
     return hs(parsed_input[0].upper(), int(parsed_input[1]))
 
-if __name__ == "__main__":
+def mode_default():
     while True:
         input_txt = input("enter roon to convert: ")
         result = hs_driver(input_txt)
         pp.PrettyPrinter(width=20).pprint(result)
+
+def mode_test(text):
+    input_txt = text
+    result = hs_driver(input_txt)
+    pp.PrettyPrinter(width=20).pprint(result)
+
+def mode_debug():
+    mode_test("ac 134")
+    mode_test("ac 135")
+    mode_test("ac 132")
+
+if __name__ == "__main__":
+    if sys.argv[1] == '1':
+        mode_debug()
+    else:
+        mode_default()
